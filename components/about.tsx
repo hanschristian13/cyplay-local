75e2b9e2876d4d925452cdc16681be34535f3495
eko <eko.darminto.90@gmail.com>
2025-01-14 21:57:58 +0700

    added icon contact

--- components/about.tsx
+++ components/about.tsx
@@ -1,12 +1,36 @@
 import React from 'react'
+import IconTelegram from '@/public/assets/telegram.svg'
+import IconWhatsapp from '@/public/assets/whatsapp.svg'
+import IconEmail from '@/public/assets/email.svg'
+import Image from 'next/image'
 
 const About = () => {
   return (
     <div className='flex items-center flex-col gap-y-3 text-white/60 text-xs mb-6'>
       <div className='flex gap-x-3'>
-        <div>@cyplay</div>
-        <div>0825-9891-3265</div>
-        <div>contact@cyplay.gg</div>
+        <div className='flex gap-x-1 items-center'>
+          <Image
+            alt='icon telegram'
+            src={IconTelegram}
+            width={16}
+            height={16}
+          />
+          @cyplay
+        </div>
+        <div className='flex gap-x-1 items-center'>
+          <Image
+            alt='icon telegram'
+            src={IconWhatsapp}
+            width={16}
+            height={16}
+          />0825-9891-3265</div>
+        <div className='flex gap-x-1 items-center'>
+          <Image
+            alt='icon telegram'
+            src={IconEmail}
+            width={16}
+            height={16}
+          />contact@cyplay.gg</div>
       </div>
       <div>© 2023 Copyright CYPlay All right reserved.</div>
     </div>
